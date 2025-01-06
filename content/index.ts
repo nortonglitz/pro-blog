aa25529207ed87c25ba060ca470e838ad2234d47
Norton Glitz <norton.glitz@gmail.com>
2025-01-06 18:33:24 -0300

    chore: insert comment on name

---
+++ content/index.ts
@@ -0,0 +1,8 @@
+export const USER_CONTENT = {
+  name: "Dwight Dunkley", // 2 names only
+  socials: {
+    linkedin: "https://www.linkedin.com/in/dwightdunkley/",
+    github: "",
+    x: ""
+  }
+}
