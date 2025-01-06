136f67509b5274002e708d0df828315de7da0f24
Norton Glitz <norton.glitz@gmail.com>
2025-01-05 21:30:08 -0300

    feat: add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,29 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
+import eslintPluginPrettier from "eslint-plugin-prettier"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: __dirname
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  {
+    ignores: [".next", "node_modules"]
+  },
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "prettier/prettier": "warn"
+    },
+    plugins: {
+      prettier: eslintPluginPrettier
+    }
+  }
+]
 
-export default eslintConfig;
+export default eslintConfig
