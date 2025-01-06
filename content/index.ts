f5ee8c519b68118300357a593170edc6c9b57e82
Norton Glitz <norton.glitz@gmail.com>
2025-01-06 19:15:43 -0300

    refactor: use type on USER_CONTENT

--- content/index.ts
+++ content/index.ts
@@ -1,4 +1,11 @@
-export const USER_CONTENT = {
+import { SocialMedia } from "@/components/icons"
+
+type UserContent = {
+  name: string
+  socials: Partial<Record<SocialMedia, string>>
+}
+
+export const USER_CONTENT: UserContent = {
   name: "Dwight Dunkley", // 2 names only
   socials: {
     linkedin: "https://www.linkedin.com/in/dwightdunkley/",
