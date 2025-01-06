f9d04f208137bce963d1b54674a67bd859dcb038
Norton Glitz <norton.glitz@gmail.com>
2025-01-06 19:15:23 -0300

    refactor: use manual socialmedia type

---
+++ components/icons/types.ts
@@ -0,0 +1,10 @@
+export type SocialMedia =
+  | "facebook"
+  | "github"
+  | "instagram"
+  | "linkedin"
+  | "pinterest"
+  | "tiktok"
+  | "x"
+  | "youtube"
+export type IconProps = React.SVGProps<SVGSVGElement>
