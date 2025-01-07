9df81df64b206303a644ceb8434c01ee170e3133
Norton Glitz <norton.glitz@gmail.com>
2025-01-07 20:27:53 -0300

    feat: add ornest font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import "./globals.css"
 import type { Metadata } from "next"
 import { USER_CONTENT } from "@/content"
-import { Sidebar } from "@/components"
 import { Jura } from "next/font/google"
+import { Onest } from "next/font/google"
+
+const onestFont = Onest({
+  subsets: ["latin"],
+  variable: "--font-onest"
+})
 
 const juraFont = Jura({
   subsets: ["latin"],
@@ -21,8 +26,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`bg-neutral-950 text-white ${juraFont.variable}`}>
-        <Sidebar />
+      <body
+        className={`
+          bg-neutral-950
+          text-white
+          ${onestFont.className}
+          ${juraFont.variable}
+          ${onestFont.variable}
+        `}
+      >
         {children}
       </body>
     </html>
