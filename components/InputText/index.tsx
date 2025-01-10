9a68991fe0c049ac2ee63560d35f022fa3f3f99e
Norton Glitz <norton.glitz@gmail.com>
2025-01-10 07:56:00 -0300

    feat: can insert icon on InputText

--- components/InputText/index.tsx
+++ components/InputText/index.tsx
@@ -1,17 +1,34 @@
-type InputTextProps = React.InputHTMLAttributes<HTMLInputElement>
+import { IconProps } from "@tabler/icons-react"
 
-export const InputText = ({ className, ...props }: InputTextProps) => (
-  <input
-    className={`
-      bg-neutral-900
-      border
-      border-neutral-800
-      px-2
-      py-1
-      focus:outline
-      focus:outline-emerald-500
-      ${className}
-    `}
-    {...props}
-  />
+type InputTextProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  icon?: React.FC<IconProps>
+}
+
+export const InputText = ({ className, type = "text", icon: Icon, ...props }: InputTextProps) => (
+  <div className="relative w-full [&:focus-within_svg]:text-emerald-500">
+    <input
+      className={`
+        ${Icon ? "pr-10" : ""}
+        w-full
+        bg-neutral-900
+        border
+        border-neutral-800
+        px-2
+        py-1
+        focus:outline
+        focus:outline-emerald-500
+        ${className}
+      `}
+      type={type}
+      {...props}
+    />
+    {Icon && (
+      <div className="flex items-center absolute top-0 bottom-0 right-0 px-2 cursor-pointer">
+        <Icon
+          stroke={1}
+          className="text-neutral-600"
+        />
+      </div>
+    )}
+  </div>
 )
