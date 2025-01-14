ad7eee00fad620bd36add20a94b51070969a4bf3
Norton Glitz <norton.glitz@gmail.com>
2025-01-13 22:42:32 -0300

    feat: include error on text area

--- components/UI/TextArea.tsx
+++ components/UI/TextArea.tsx
@@ -1,18 +1,22 @@
-type TextAreaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>
+import clsx from "clsx"
 
-export const TextArea = ({ ...props }: TextAreaProps) => {
+type TextAreaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
+  error?: string
+  resize?: boolean
+}
+
+const baseStyles = "border px-2 py-1 w-full bg-neutral-900 "
+const standardStyles = "border-neutral-700"
+const errorStyles = "border-red-600"
+
+export const TextArea = ({ resize = false, error, ...props }: TextAreaProps) => {
   return (
-    <textarea
-      className="
-        bg-neutral-900
-        border
-        border-neutral-700
-        px-2
-        py-1
-        w-full
-        resize-none
-      "
-      {...props}
-    />
+    <div>
+      <textarea
+        className={clsx(baseStyles, error ? errorStyles : standardStyles, !resize && "resize-none")}
+        {...props}
+      />
+      {error && <p className="text-sm mt-0.5 text-red-600">{error}</p>}
+    </div>
   )
 }
