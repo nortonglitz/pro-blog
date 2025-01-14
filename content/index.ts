28bbb4199d60c76ef24de93986fd97485f061eed
Norton Glitz <norton.glitz@gmail.com>
2025-01-14 00:28:42 -0300

    feat: include random messages

--- content/index.ts
+++ content/index.ts
@@ -85,3 +85,85 @@ export const POSTS: Post[] = [
     image_url: "/posts/tech-sustain-future.webp"
   }
 ]
+
+export type Message = (typeof MESSAGES)[0]
+
+export const MESSAGES = [
+  {
+    from: "gabriel14@gregory-morales.com",
+    receivedOn: "2025-01-03 09:12:31",
+    subject: "Throughout difference paper beautiful seem.",
+    read: false,
+    content: "That tax almost page instead."
+  },
+  {
+    from: "stephanie93@harris-hahn.com",
+    receivedOn: "2025-01-01 17:34:28",
+    subject: "Common court manage degree daughter language.",
+    read: false,
+    content:
+      "Cut shake place dark phone amount close. Kid tough director drop some job task cup. Professor change cover ahead."
+  },
+  {
+    from: "rosemcintyre@hotmail.com",
+    receivedOn: "2025-01-08 17:41:06",
+    subject: "Consumer law base with media seem third.",
+    read: true,
+    content:
+      "Grow inside newspaper husband wind indicate. Act grow rather south prove evening. Front lay early wind oil point."
+  },
+  {
+    from: "joneschristina@yahoo.com",
+    receivedOn: "2025-01-08 20:34:22",
+    subject: "Product a imagine worker.",
+    read: false,
+    content:
+      "Amount organization center red fish political about. Worker fill ten morning firm since share. Central dark fund."
+  },
+  {
+    from: "nguyenbrian@yahoo.com",
+    receivedOn: "2025-01-09 04:36:53",
+    subject: "Event boy medical over crime they.",
+    read: false,
+    content:
+      "Wide back late. Week heart score until relationship grow. System tend describe walk series."
+  },
+  {
+    from: "zcervantes@gmail.com",
+    receivedOn: "2025-01-05 08:22:33",
+    subject: "Somebody sound heavy eat.",
+    read: true,
+    content: "Meet policy player skill college during capital."
+  },
+  {
+    from: "sanchezcristina@martinez.com",
+    receivedOn: "2025-01-03 21:47:29",
+    subject: "Impact everybody trade tree expert quite.",
+    read: false,
+    content: "On pick member determine defense. Region heavy should purpose need."
+  },
+  {
+    from: "ythomas@underwood.com",
+    receivedOn: "2025-01-11 13:38:27",
+    subject: "Before foreign feeling fund interesting.",
+    read: true,
+    content:
+      "Two page for democratic study media once. Your bank table including away southern if. Fight quite red position."
+  },
+  {
+    from: "johnwhite@marquez.com",
+    receivedOn: "2025-01-09 11:12:04",
+    subject: "Have shoulder street project human.",
+    read: true,
+    content:
+      "Free plant step evening ten line. Significant painting role challenge why while. Career figure central he."
+  },
+  {
+    from: "bcarlson@davis.com",
+    receivedOn: "2025-01-04 18:57:09",
+    subject: "Picture article study that wall same.",
+    read: true,
+    content:
+      "Ball story from case tonight. Interview here with recent. It break study different street interview part fund."
+  }
+]
