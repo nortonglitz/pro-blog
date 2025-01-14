1f7dda1ff30774fce93b488c89cf61dc9a94b4d1
Norton Glitz <norton.glitz@gmail.com>
2025-01-13 22:42:42 -0300

    feat: include error on inputtext

--- components/UI/InputText.tsx
+++ components/UI/InputText.tsx
@@ -1,9 +1,17 @@
+import clsx from "clsx"
+
 type InputTextProps = React.InputHTMLAttributes<HTMLInputElement> & {
   onEnter?: (e: React.KeyboardEvent<HTMLInputElement>) => void
+  error?: string
 }
+
+const baseStyles = "border px-2 py-1 w-full border bg-neutral-900"
+const standardStyles = "border-neutral-700"
+const errorStyles = "border-red-600"
 
 export const InputText = ({
   type = "text",
+  error,
   className,
   onEnter,
   onKeyDown: onKeyDownExternal,
@@ -17,19 +25,14 @@ export const InputText = ({
   }
 
   return (
-    <input
-      onKeyDown={handleKeyDown}
-      className={`
-        bg-neutral-900
-        border
-        border-neutral-700
-        px-2
-        py-1
-        w-full
-        ${className}
-      `}
-      type={type}
-      {...props}
-    />
+    <div className="w-full">
+      <input
+        onKeyDown={handleKeyDown}
+        className={clsx(baseStyles, error ? errorStyles : standardStyles, className)}
+        type={type}
+        {...props}
+      />
+      {error && <p className="text-sm mt-0.5 text-red-600">{error}</p>}
+    </div>
   )
 }
