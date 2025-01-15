22994480b2e213328f6f8e33a254b1b4af24840d
Norton Glitz <norton.glitz@gmail.com>
2025-01-15 02:54:50 -0300

    feat: messages mobile responsive

--- app/dashboard/messages/MessageModal.tsx
+++ app/dashboard/messages/MessageModal.tsx
@@ -13,10 +13,10 @@ export const MessageModal = ({ message, isOpen, onClose }: MessageModalProps) =>
   if (!isOpen || !message) return null
 
   return (
-    <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center z-30 bg-black/60 backdrop-blur-sm">
+    <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center z-30 bg-black/70 backdrop-blur-sm">
       <article className="bg-neutral-900 p-4 flex flex-col border-neutral-800 border mx-1">
         <header className="border-b border-neutral-600 pb-2 flex justify-between gap-10 items-center">
-          <h1 className="text-xl">{message.subject}</h1>
+          <h1 className="text-lg sm:text-xl md:text-2xl">{message.subject}</h1>
           <Button
             onClick={() => onClose?.()}
             variant="ghost"
@@ -25,9 +25,9 @@ export const MessageModal = ({ message, isOpen, onClose }: MessageModalProps) =>
             <IconX />
           </Button>
         </header>
-        <div className="flex justify-between items-baseline gap-10 mt-2 text-neutral-400 mb-4">
-          <p>{message.from}</p>
-          <p className="text-sm">{formatDistanceToNow(message.receivedOn) + " ago"}</p>
+        <div className="text-sm flex justify-between items-baseline gap-10 mt-2 text-neutral-400 mb-4">
+          <p className="flex-1 text-sm">{message.from}</p>
+          <p className="text-xs sm:text-sm">{formatDistanceToNow(message.receivedOn) + " ago"}</p>
         </div>
         <p className="mt-2 p-4 bg-neutral-800 text-justify max-w-prose">{message.content}</p>
       </article>
