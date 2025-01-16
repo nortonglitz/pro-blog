c6f52f17a5bdd2dc548f6140298ed15ee8dea078
Norton Glitz <norton.glitz@gmail.com>
2025-01-15 22:56:29 -0300

    fix: fix types on jobs and socials columns

--- schemas/drizzle/user-info.ts
+++ schemas/drizzle/user-info.ts
@@ -4,8 +4,8 @@ export const userInfoTable = pgTable("user_info", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(), // ID único para o usuário
   first_name: varchar({ length: 100 }).notNull(), // Primeiro nome do usuário
   last_name: varchar({ length: 100 }).notNull(), // Último nome do usuário
-  jobs: jsonb().notNull(), // Lista de trabalhos
-  socials: jsonb().notNull(), // Links de redes sociais
+  jobs: jsonb("jobs").$type<string[]>().notNull(), // Lista de trabalhos
+  socials: jsonb().$type<Record<string, string>>().notNull(), // Links de redes sociais
   about: text().notNull(), // Texto sobre o usuário
   updated_at: timestamp().defaultNow().notNull(),
   created_at: timestamp().defaultNow().notNull()
