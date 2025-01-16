49fd68ce3f2784c834d57a4baf3170888ca126db
Norton Glitz <norton.glitz@gmail.com>
2025-01-15 23:41:39 -0300

    feat: add getUserInfo action

---
+++ db/actions/user-info/getUserInfo.ts
@@ -0,0 +1,15 @@
+"use server"
+
+import db from "@/db"
+import { desc } from "drizzle-orm"
+import { userInfoTable } from "@/schemas/drizzle"
+
+export const getUserInfo = async () => {
+  try {
+    const userInfo = await db.select().from(userInfoTable).orderBy(desc(userInfoTable.id)).limit(1)
+    return userInfo[0] || null
+  } catch (err) {
+    console.error("Error fetching user info:", err)
+    throw new Error("Faled to fetch user info")
+  }
+}
