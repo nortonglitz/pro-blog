8bb6094f34a6bdeaaefb33aa71f5988b9baa0604
Norton Glitz <norton.glitz@gmail.com>
2025-01-16 01:00:03 -0300

    styles: include disabled styles and use clsx

--- components/UI/Chip.tsx
+++ components/UI/Chip.tsx
@@ -1,5 +1,6 @@
 import { TablerIcon } from "@tabler/icons-react"
 import React from "react"
+import clsx from "clsx"
 
 type ChipProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   textClassName?: string
@@ -16,6 +17,11 @@ type ChipProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   onRightIconClick?: () => void
 }
 
+const chipStyles = "flex items-center border border-neutral-700"
+const disabledStyles = "disabled:cursor-not-allowed disabled:opacity-50"
+const buttonStyles = "cursor-pointer hover:bg-neutral-800"
+const iconStyles = "flex items-center px-2 h-full"
+
 export const Chip = ({
   as,
   rightIcon: RightIcon,
@@ -31,48 +37,38 @@ export const Chip = ({
   children,
   ...props
 }: ChipProps) => {
-  const buttonClassName = "cursor-pointer hover:bg-neutral-800"
-
   return React.createElement(as === "button" ? "button" : "span", {
     type: as === "button" ? "button" : undefined,
-    className: `
-          flex
-          items-center
-          border
-          border-neutral-700
-          ${as === "button" ? buttonClassName : ""}
-          ${RightIcon ? "" : "pr-2"}
-          ${LeftIcon ? "" : "pl-2"}
-          ${className}`,
+    className: clsx(
+      chipStyles,
+      as === "button" && [buttonStyles, disabledStyles],
+      RightIcon && "pl-2",
+      LeftIcon && "pr-2",
+      className
+    ),
     children: (
       <>
         {LeftIcon &&
           React.createElement(leftIconAs === "button" ? "button" : "span", {
             onClick: onLeftIconClick,
-            className: `
-              flex
-              items-center
-              px-2
-              h-full
-              mr-1
-              ${leftIconAs === "button" ? buttonClassName : ""}
-              ${leftIconClassName}
-            `,
+            className: clsx(
+              iconStyles,
+              "mr-1",
+              leftIconAs === "button" && [buttonStyles, disabledStyles],
+              leftIconClassName
+            ),
             children: <LeftIcon size="1rem" />
           })}
-        <span className={`py-1 ${textClassName}`}>{children}</span>
+        <span className={clsx("py-1", textClassName)}>{children}</span>
         {RightIcon &&
           React.createElement(rightIconAs === "button" ? "button" : "span", {
             onClick: onRightIconClick,
-            className: `
-              flex
-              items-center
-              px-2
-              h-full
-              ml-1
-              ${rightIconAs === "button" ? buttonClassName : ""}
-              ${rightIconClassName}
-            `,
+            className: clsx(
+              iconStyles,
+              "ml-1",
+              rightIconAs === "button" && [buttonStyles, disabledStyles],
+              rightIconClassName
+            ),
             children: <RightIcon size="1rem" />
           })}
       </>
