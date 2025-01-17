b1d21bef93d3bb852a71de7fdef0a8f17785b302
Norton Glitz <norton.glitz@gmail.com>
2025-01-16 23:51:21 -0300

    feat: add deletePost action

---
+++ db/actions/posts/deletePost.ts
@@ -0,0 +1,16 @@
+"use server"
+
+import db from "@/db"
+import { postsTable } from "@/schemas/drizzle"
+import { eq } from "drizzle-orm"
+
+export const deletePost = async (id: number) => {
+  try {
+    const result = await db.delete(postsTable).where(eq(postsTable.id, id))
+
+    return result.rowCount > 0
+  } catch (err) {
+    console.error("Error deleting post:", err)
+    throw new Error("Failed to delete post")
+  }
+}
