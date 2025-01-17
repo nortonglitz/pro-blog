7c71dd1705b29275521b942aabe92cb2fb89ef84
Norton Glitz <norton.glitz@gmail.com>
2025-01-17 01:46:22 -0300

    refactor: max title size on posts 100 chars

--- schemas/drizzle/posts.ts
+++ schemas/drizzle/posts.ts
@@ -4,7 +4,7 @@ import { QuillDelta } from "@/types"
 export const postsTable = pgTable("posts", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   image_url: varchar({ length: 255 }).notNull(),
-  title: varchar({ length: 255 }).notNull(),
+  title: varchar({ length: 100 }).notNull(),
   content: jsonb().$type<QuillDelta>().notNull(),
   updated_at: timestamp().defaultNow().notNull(),
   created_at: timestamp().defaultNow().notNull()
