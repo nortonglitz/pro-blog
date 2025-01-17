533bb71aba73f0f22268cffed97f79b3b20bf290
Norton Glitz <norton.glitz@gmail.com>
2025-01-16 23:18:57 -0300

    refactor: use types folder to get QuillDelta

--- schemas/drizzle/posts.ts
+++ schemas/drizzle/posts.ts
@@ -1,10 +1,11 @@
 import { integer, pgTable, timestamp, varchar, jsonb } from "drizzle-orm/pg-core"
+import { QuillDelta } from "@/types"
 
 export const postsTable = pgTable("posts", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   image_url: varchar({ length: 255 }).notNull(),
   title: varchar({ length: 255 }).notNull(),
-  content: jsonb().notNull(),
+  content: jsonb().$type<QuillDelta>().notNull(),
   updated_at: timestamp().defaultNow().notNull(),
   created_at: timestamp().defaultNow().notNull()
 })
