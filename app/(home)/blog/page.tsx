54c5c87f093e7892f342764f6d4120c23d13e225
Norton Glitz <norton.glitz@gmail.com>
2025-01-19 18:42:46 -0300

    feat: get posts from db

--- app/(home)/blog/page.tsx
+++ app/(home)/blog/page.tsx
@@ -1,11 +1,13 @@
-import { POSTS } from "@/content"
 import Link from "next/link"
-import { useId } from "react"
 import { BlogCard } from "../BlogCard"
 import { InputText } from "@/components"
 import { IconSearch, IconX } from "@tabler/icons-react"
+import { getPosts } from "@/db/actions/posts"
+import { extractTextFromDeltaOps } from "@/libs/quill"
 
-export default function Home() {
+export default async function Home() {
+  const posts = await getPosts(20)
+
   return (
     <main className="px-2 md:px-5 h-screen overflow-y-auto py-20">
       <h1
@@ -24,31 +26,15 @@ export default function Home() {
         Blog
       </h1>
       <section>
-        <div className="m-auto mb-10 max-w-prose">
-          <InputText
-            type="text"
-            icon={IconSearch}
-            placeholder="Looking for something specific?"
-          />
-        </div>
-        <p
-          className="text-neutral-500 mb-5 flex gap-1 items-center cursor-pointer"
-          title="Delete search"
-        >
-          Showing results for: <span className="text-emerald-500">technology</span>
-          <IconX size={16} />
-        </p>
-      </section>
-      <section>
         <div className="flex flex-col lg:grid lg:grid-cols-2 2xl:grid-cols-3 gap-10">
-          {POSTS.map(({ title, content, image_url }) => (
+          {posts.map(({ id, title, content, image_url }) => (
             <Link
-              href="/blog/posts/2"
-              key={`blog-card-${useId()}`}
+              href={`/blog/posts/${id}`}
+              key={`blog-card-${id}`}
             >
               <BlogCard
                 title={title}
-                content={content}
+                content={extractTextFromDeltaOps(content)}
                 imgUrl={image_url}
               />
             </Link>
