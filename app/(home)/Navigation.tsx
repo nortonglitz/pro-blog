c3a0262252296f38d20e3f5b9abf989e054df19c
Norton Glitz <norton.glitz@gmail.com>
2025-01-19 19:40:45 -0300

    feat: get name from db

--- app/(home)/Navigation.tsx
+++ app/(home)/Navigation.tsx
@@ -1,15 +1,13 @@
-"use client"
-
 import { USER_CONTENT } from "@/content"
 import Link from "next/link"
 import { IconSocialLink } from "@/components"
-import { useState } from "react"
 import { SocialMedia } from "@/components/icons"
 import { LINKS } from "@/content"
 import { MenuMobile } from "./MenuMobile"
+import { getUserInfo } from "@/db/actions/user-info"
 
-export const Navigation = () => {
-  const [activeLink, setActiveLink] = useState(LINKS[0])
+export const Navigation = async () => {
+  const userInfo = await getUserInfo()
   const linksId = LINKS.map(title => title.replace(" ", "-").toLocaleLowerCase())
 
   return (
@@ -28,27 +26,32 @@ export const Navigation = () => {
           bg-stone-950
         "
       >
-        <header className="px-4 mx-auto">
-          {USER_CONTENT.name.split(" ").map((name, i) => (
-            <h3
-              key={`name-title-${i}`}
-              className="
-                text-4xl
-                font-jura
-                w-44
+        <header
+          className="
+            px-4
+            mx-auto
 
-                first:text-left
-                last:text-right
-              "
-            >
-              <span className="text-emerald-500 font-semibold">{name[0]}</span>
-              {name.slice(1)}
-            </h3>
-          ))}
+            [&_h3]:text-4xl
+            [&_h3]:font-jura
+            [&_h3]:w-44
+
+            [&_span]:text-emerald-500
+            [&_span]:font-semibold
+          "
+        >
+          <h3>
+            <span>{userInfo?.first_name[0]}</span>
+            {userInfo?.first_name.slice(1)}
+          </h3>
+          <h3 className="text-right">
+            <span>{userInfo?.last_name[0]}</span>
+            {userInfo?.last_name.slice(1)}
+          </h3>
         </header>
         <nav>
           <ul>
             {LINKS.map((title, i) => (
+              /*activeLink === title ? "border-r-emerald-500 bg-white/10 font-semibold" : "border-r-transparent"*/
               <li
                 key={`nav-item-${i}`}
                 className={`
@@ -61,12 +64,10 @@ export const Navigation = () => {
                     pointer-events-none
 
                     last:border-b-0
-                    ${activeLink === title ? "border-r-emerald-500 bg-white/10 font-semibold" : "border-r-transparent"}
                   `}
               >
                 <Link
                   className="transition w-full h-full block py-3 pointer-events-auto hover:scale-110"
-                  onClick={() => setActiveLink(title)}
                   href={`/#${linksId[i]}`}
                   key={`side-nav-${i}`}
                 >
