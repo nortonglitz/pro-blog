6296adf224efafdc457e5012cba1e89661732b73
Norton Glitz <norton.glitz@gmail.com>
2025-01-19 16:42:43 -0300

    refactor: use op direct from quill package

--- schemas/drizzle/posts.ts
+++ schemas/drizzle/posts.ts
@@ -1,11 +1,11 @@
 import { integer, pgTable, timestamp, varchar, jsonb } from "drizzle-orm/pg-core"
-import { QuillOps } from "@/types"
+import { Op } from "quill"
 
 export const postsTable = pgTable("posts", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   image_url: varchar({ length: 255 }).notNull(),
   title: varchar({ length: 100 }).notNull(),
-  content: jsonb().$type<QuillOps>().notNull(),
+  content: jsonb().$type<Op[]>().notNull(),
   updated_at: timestamp().defaultNow().notNull(),
   created_at: timestamp().defaultNow().notNull()
 })
