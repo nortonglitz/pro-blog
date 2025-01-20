af03ac5cf57faf6105867ce53dfebdf1bd9880bd
Norton Glitz <norton.glitz@gmail.com>
2025-01-19 23:08:31 -0300

    feat: add spinner sizes

--- components/UI/Spinner.tsx
+++ components/UI/Spinner.tsx
@@ -1,17 +1,25 @@
-export const Spinner = () => {
+import clsx from "clsx"
+
+type SpinnerProps = {
+  size?: "sm" | "md" | "lg" | "xl" | "2xl" | "3xl"
+  className?: string
+}
+
+const sizes = {
+  sm: "w-4 h-4",
+  md: "w-6 h-6",
+  lg: "w-10 h-10",
+  xl: "w-16 h-16",
+  "2xl": "w-24 h-24",
+  "3xl": "w-32 h-32"
+}
+
+export const Spinner = ({ size = "md", className }: SpinnerProps) => {
   return (
-    <div className="relative w-fit h-fit">
+    <div className={clsx("relative w-fit h-fit", className)}>
+      <div className={clsx(sizes[size], "border-2 rounded-full border-white/80")} />
       <div
         className="
-          w-6
-          h-6
-          border-2
-          rounded-full
-          border-white/80
-        "
-      />
-      <div
-        className={`
           absolute 
           border-2
           rounded-full
@@ -20,11 +28,8 @@ export const Spinner = () => {
           border-l-transparent
           animate-spin
           border-black/80
-          top-0
-          left-0
-          right-0
-          bottom-0
-        `}
+          inset-0
+        "
       />
     </div>
   )
