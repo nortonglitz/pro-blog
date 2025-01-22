67389855bea453d2a5fdfbc45217925507c4a0f7
Norton Glitz <norton.glitz@gmail.com>
2025-01-22 01:13:36 -0300

    feat: add form validation and login integration

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -4,17 +4,27 @@ import { Button, InputText } from "@/components/UI"
 import { IconDashboard } from "@tabler/icons-react"
 import { useZodForm } from "@/hooks"
 import { loginFormSchema, LoginFormSchema } from "@/schemas/validations"
+import { login } from "@/auth/actions/login"
+import { useState } from "react"
+import toast from "react-hot-toast"
 
 export default function Login() {
+  const [isLoading, setIsLoading] = useState(false)
   const {
     register,
     handleSubmit,
     formState: { errors }
   } = useZodForm({ schema: loginFormSchema })
 
-  const onSubmit = (data: LoginFormSchema) => {
-    console.log(data)
+  const onSubmit = async (formData: LoginFormSchema) => {
+    setIsLoading(true)
+    const isValid = await login(formData)
+    if (!isValid) {
+      toast.error("Invalid credentials")
+    }
+    setIsLoading(false)
   }
+
   return (
     <main className="flex flex-col gap-10 justify-center items-center h-screen">
       <header>
@@ -34,17 +44,24 @@ export default function Login() {
         className="gap-4 flex flex-col mb-32 max-w-72 w-full"
       >
         <InputText
+          disabled={isLoading}
           placeholder="Username"
           {...register("username")}
           error={errors.username?.message}
         />
         <InputText
+          disabled={isLoading}
           type="password"
           placeholder="Password"
           {...register("password")}
           error={errors.password?.message}
         />
-        <Button type="submit">Submit</Button>
+        <Button
+          loading={isLoading}
+          type="submit"
+        >
+          Submit
+        </Button>
       </form>
     </main>
   )
